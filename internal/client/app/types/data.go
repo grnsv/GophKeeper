4767006e3e03f03096f504d61a82087ee4f32632
grnsv <grin.sv@gmail.com>
2025-06-29 23:15:25 +0300

    refactor: start refactoring metadata

--- internal/client/app/types/data.go
+++ internal/client/app/types/data.go
@@ -4,4 +4,23 @@ type Credentials struct {
 	Resource string
 	Login    string
 	Password string
+	Metadata map[string]string
+}
+
+type Text struct {
+	Text     string
+	Metadata map[string]string
+}
+
+type Binary struct {
+	Binary   []byte
+	Metadata map[string]string
+}
+
+type Card struct {
+	CardNumber string
+	EXP        string
+	CVV        string
+	CardHolder string
+	Metadata   map[string]string
 }
